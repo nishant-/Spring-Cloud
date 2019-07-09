c834e9890273c675478435e302571378f27dce9f
Nishant Bhadani <itisnishant@gmail.com>
2019-07-10 07:11:01 +1000

    comment

--- eureka-naming-server/src/main/java/eureka/naming/server/EurekaNamingServerApplication.java
+++ eureka-naming-server/src/main/java/eureka/naming/server/EurekaNamingServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //this enables the naming server
 public class EurekaNamingServerApplication {
 
     public static void main(String[] args) {
