b8f4c42ef8a0e62d3fb10e5b9cb1381fefd22f21
Nishant Bhadani <itisnishant@gmail.com>
2019-07-12 07:01:52 +1000

    Hystrix example added.

--- limit-service/src/main/java/limit/service/LimitServiceApplication.java
+++ limit-service/src/main/java/limit/service/LimitServiceApplication.java
@@ -2,8 +2,12 @@ package limit.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix //enables hystrix on all controller methods
 public class LimitServiceApplication {
 
     public static void main(String[] args) {
--- limit-service/src/main/java/limit/service/controller/LimitsConfigurationController.java
+++ limit-service/src/main/java/limit/service/controller/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package limit.service.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import limit.service.bean.LimitConfiguration;
 import limit.service.config.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,16 @@ public class LimitsConfigurationController {
                maximum(configuration.getMaximum()).
                minimum(configuration.getMinimum()).build();
        return limitConfiguration;
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration() {
+        throw new RuntimeException("config not available");
+    }
+
+    //this is the method which will be executed by Hystrix when exception occurs
+    public LimitConfiguration fallbackRetrieveConfiguration() {
+       return  LimitConfiguration.builder().maximum(1).minimum(12311).build();
     }
 }
